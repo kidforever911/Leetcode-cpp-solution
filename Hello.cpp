ca0092e1622bd61741868c8462aca2618b32b6ea
kidforever <pang_huadong0811@outlook.com>
2022-11-14 09:57:07 -0500

    修改：Hello.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -6,6 +6,7 @@ using namespace std;
 
 int main()
 {
+    int a;
     vector<string> msg{"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
     for (const string &word : msg)
     {
